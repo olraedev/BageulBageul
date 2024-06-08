effd10939788db64d555246d8d84e7e1cc78bcd0
SSangRRae <pepe0003@kakao.com>
2024-06-08 15:47:37 +0900

    Initial Commit

---
+++ BageulBageul/BageulBageulApp.swift
@@ -0,0 +1,17 @@
+//
+//  BageulBageulApp.swift
+//  BageulBageul
+//
+//  Created by SangRae Kim on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct BageulBageulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
