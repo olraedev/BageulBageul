0ea6a31f1e18b6e6cf25aa6c1e6e0d3d2e07013c
SSangRRae <pepe0003@kakao.com>
2024-06-17 16:57:56 +0900

    Design: Color, Image Assets

---
+++ BageulBageul/Font/FontAsset.swift
@@ -0,0 +1,16 @@
+//
+//  FontAsset.swift
+//  BageulBageul
+//
+//  Created by SangRae Kim on 6/17/24.
+//
+
+import SwiftUI
+
+extension Font {
+    static let customTitle1: Font = .custom("SFPro-Regular", size: 22).weight(.bold)
+    static let customTitle2: Font = .custom("SFPro-Regular", size: 14).weight(.bold)
+    static let customBodyBold: Font = .custom("SFPro-Regular", size: 13).weight(.bold)
+    static let customBody: Font = .custom("SFPro-Regular", size: 13)
+    static let customCaption: Font = .custom("SFPro-Regular", size: 22)
+}
